027a811d5de564ed6af6d385e3a51af932fec989
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-04-11 19:54:30 +0200

    Zalozeni projektu v depositari

---
+++ Opakovani/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Opakovani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vitejte u kurzu C#2");
+        }
+    }
+}
