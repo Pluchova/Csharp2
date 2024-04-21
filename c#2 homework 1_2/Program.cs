1a45252d15a8020ddeb5f8709e9f557823f71b20
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-04-21 11:29:43 +0200

    Create Program.cs

---
+++ c#2 homework 1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace c_2_homework_1_2
+{
+    internal class Program
+    { /*
+Vytvořte třídu Lucistnik, ktera bude mit vlastnost PocetSipu a metodu Vystrel. PocetSipu muze byt nastaven treba na 10.
+Pokud bude mit dost sipu, napise metoda Vystrel na konzoli text: Vzdy se strefim primo do prostred!
+Metoda Vystrel uvnitr s kazdym vystrelem snizi pocet sipu.
+Pokud bude pocet 0, metoda Vystrel vypise "Nemam sipy".
+Napiste program, ktery vytvori lucistnika a vystreli vsechny sipy.
+*/
+        static void Main(string[] args)
+        {
+            Lucistnik Robin = new Lucistnik(10);
+
+            while (Robin.PocetSipu >= 0)
+            {
+                Robin.Vystrel();
+
+            }
+
+            
+        }
+    }
+}
