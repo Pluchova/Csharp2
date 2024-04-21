57f4daea7fe2c489f8a4440ab475f1204ef2bae8
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-04-21 11:29:45 +0200

    Create Lucistnik.cs

---
+++ c#2 homework 1_2/Lucistnik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_2_homework_1_2
+{
+    internal class Lucistnik
+    {
+        public int PocetSipu;
+        public Lucistnik(int pocetSipu)
+        {
+            PocetSipu = pocetSipu;
+        }
+        public void Vystrel()
+        {
+            if (PocetSipu > 0)
+            {
+                Console.WriteLine("Vzdy se strefim primo do prostred!");
+                PocetSipu--;
+            }
+            else
+            {
+                Console.WriteLine("Nemam sipy");
+            }
+        }
+    }
+}
