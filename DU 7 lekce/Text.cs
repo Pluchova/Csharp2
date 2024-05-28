7d0c0b971887ae1a069935eeac0e05be2aa87f2b
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-05-28 16:25:08 +0200

    Create Text.cs

---
+++ DU 7 lekce/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DU_7_lekce
+{
+    internal class Text : GrafickyObjekt
+    {       
+        public string VypsanyText { get; set; }
+        public override void Vykreslit()
+        {
+            NastavBarvu();
+            Console.WriteLine(VypsanyText);
+            ResetBarva();
+        }
+
+    }
+}
