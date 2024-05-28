e51979cc4bc904e24776f56c340e093997729c5d
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-05-28 16:25:13 +0200

    Create Obdelnik.cs

---
+++ DU 7 lekce/Obdelnik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DU_7_lekce
+{
+    internal class Obdelnik : GrafickyObjekt
+    {   public int Sirka { get; set; }
+        public int Vyska { get; set; }
+        
+
+        public override void Vykreslit()
+        {
+            NastavBarvu();
+            for (int i = 0; i < Vyska; i++)
+            {
+                for (int j = 0; j < Sirka; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+
+            }
+            ResetBarva();
+        }
+
+
+    }
+}
