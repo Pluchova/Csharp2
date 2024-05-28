fa279a26b15492125ea9ff7c7e42fd8cb6c8d9c1
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-05-28 16:25:15 +0200

    Create GrafickyObjekt.cs

---
+++ DU 7 lekce/GrafickyObjekt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DU_7_lekce
+{
+    internal class GrafickyObjekt
+    {
+        
+        public virtual void Vykreslit()
+        {
+            
+            Console.WriteLine("X");
+        }
+        public string Barva { get; set; }
+
+        public void NastavBarvu()
+        {           
+            Console.ForegroundColor = Enum.TryParse<ConsoleColor>(Barva, out ConsoleColor parsedBarva) ? parsedBarva : ConsoleColor.Gray;
+        }
+
+       public void ResetBarva()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
