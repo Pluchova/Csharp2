2a97e7cac8e32fd77832ffb06485688a961d4183
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-05-28 16:25:05 +0200

    Create Trojuhelnik.cs

---
+++ DU 7 lekce/Trojuhelnik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DU_7_lekce
+{
+    internal class Trojuhelnik : GrafickyObjekt
+    {
+        public int Vyska { get; set; }
+
+        public override void Vykreslit()
+        {
+            NastavBarvu();
+            for (int i = 1; i <= Vyska; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            ResetBarva();
+        }
+        }
+       
+}
