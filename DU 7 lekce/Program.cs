60751b2767e47b4a409980fa7a4cfc6ab0699a36
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-05-28 16:25:10 +0200

    Create Program.cs

---
+++ DU 7 lekce/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DU_7_lekce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<GrafickyObjekt> objekty = new List<GrafickyObjekt>
+            {   new GrafickyObjekt(),
+                new Obdelnik { Sirka = 3, Vyska = 2, Barva = "Red" },
+                new Trojuhelnik { Vyska = 3, Barva ="Green" },
+                new Text { VypsanyText = "Ahoj", Barva ="Magenta" }
+            };
+
+            foreach (var objekt in objekty)
+            {
+                objekt.Vykreslit();
+                Console.WriteLine();
+            }
+        }
+    }
+}
