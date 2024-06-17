ede128e1234fe2194b4860142f16f0920ca72973
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-06-17 11:08:45 +0200

    úprava databáze

--- Pole Dance projekt/dbo.Prvky.data.sql
+++
@@ -1,7 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Prvky] ON
-INSERT INTO [dbo].[Prvky] ([Id], [Nazev], [Obtiznost], [Inverted]) VALUES (1, N'Basic Spin', N'Beginner', 0)
-INSERT INTO [dbo].[Prvky] ([Id], [Nazev], [Obtiznost], [Inverted]) VALUES (2, N'Inverted V', N'Intermediate', 1)
-INSERT INTO [dbo].[Prvky] ([Id], [Nazev], [Obtiznost], [Inverted]) VALUES (3, N'Shoulder Mount', N'Intermediate', 1)
-INSERT INTO [dbo].[Prvky] ([Id], [Nazev], [Obtiznost], [Inverted]) VALUES (4, N'Fireman Spin', N'Beginner', 0)
-INSERT INTO [dbo].[Prvky] ([Id], [Nazev], [Obtiznost], [Inverted]) VALUES (5, N'Iron X', N'Advanced', 1)
-SET IDENTITY_INSERT [dbo].[Prvky] OFF
