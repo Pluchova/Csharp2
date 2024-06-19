bc779dd9035645843c687cd7e1bed446ad5cb335
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-06-19 09:09:34 +0200

    Delete Pole Dance projekt directory

--- Pole Dance projekt/dbo.Prvky.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Prvky] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [Nazev]            NVARCHAR (100) NULL,
-    [Obtiznost] NVARCHAR (50)  NULL,
-    [Inverted]    BIT            NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
