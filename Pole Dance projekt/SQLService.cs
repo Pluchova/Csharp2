8ea7b89e9fee86010246af278809ec341d876dd6
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-06-19 09:16:59 +0200

    Create SQLService.cs

---
+++ Pole Dance projekt/SQLService.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Data.SQLite;
+namespace Pole_Dance_projekt
+{
+    public class SqlService : IDataService
+    {
+        private string connectionString;
+        public SqlService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public IEnumerable<string> GetPrvky(string obtiznost, bool includeInverted)
+        {
+            List<string> prvky = new List<string>();
+            using (var pripojeni = new SQLiteConnection(connectionString))
+            {
+                pripojeni.Open();
+                string query = "SELECT Nazev FROM Prvky WHERE Obtiznost = @obtiznost AND (Inverted = 0 OR (Inverted = 1 AND @includeInverted = 1))";
+                var prikaz = pripojeni.CreateCommand();
+                prikaz.CommandText = query;
+                prikaz.Parameters.AddWithValue("@obtiznost", obtiznost);
+                prikaz.Parameters.AddWithValue("@includeInverted", includeInverted ? 1 : 0);
+                var adapter = new SQLiteDataAdapter(prikaz);
+                var table = new DataTable();
+                adapter.Fill(table);
+                foreach (DataRow row in table.Rows)
+                {
+                    prvky.Add(row["Nazev"].ToString());
+                }
+            }
+            return prvky;
+        }
+        public IEnumerable<string> GetObtiznosti()
+        {
+            List<string> obtiznosti = new List<string>();
+            using (var pripojeni = new SQLiteConnection(connectionString))
+            {
+                pripojeni.Open();
+                var prikaz = pripojeni.CreateCommand();
+                prikaz.CommandText = "SELECT Obtiznost FROM (" +
+                    "SELECT DISTINCT Obtiznost, " +
+                    "CASE " +
+                    "WHEN Obtiznost = 'Beginner' THEN 1 " +
+                    "WHEN Obtiznost = 'Intermediate' THEN 2 " +
+                    "WHEN Obtiznost = 'Advanced' THEN 3 " +
+                    "ELSE 4 END AS SortOrder " +
+                    "FROM Prvky) AS InnerQuery " +
+                    "ORDER BY SortOrder";
+                var adapter = new SQLiteDataAdapter(prikaz);
+                var table = new DataTable();
+                adapter.Fill(table);
+                foreach (DataRow row in table.Rows)
+                {
+                    obtiznosti.Add(row["Obtiznost"].ToString());
+                }
+            }
+            return obtiznosti;
+        }
+    }
+}
