78f0fd703dd96675ae35668c7f5d9ae448fd4b26
Pluchova <156010311+Pluchova@users.noreply.github.com>
2024-06-19 09:16:34 +0200

    Create IDataService.cs

---
+++ Pole Dance projekt/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pole_Dance_projekt
+{
+    public interface IDataService
+    {
+        IEnumerable<string> GetPrvky(string obtiznost, bool includeInverted);
+        IEnumerable<string> GetObtiznosti();
+    }
+}
+
